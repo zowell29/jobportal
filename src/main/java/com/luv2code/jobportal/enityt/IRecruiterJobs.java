abc401ed5e868913be521984a30121348453dac3
zowell <67911888+zowell29@users.noreply.github.com>
2026-05-12 11:18:43 +0300

    Recruiter Dashboard - display recruiter's list of jobs

---
+++ src/main/java/com/luv2code/jobportal/enityt/IRecruiterJobs.java
@@ -0,0 +1,17 @@
+package com.luv2code.jobportal.enityt;
+
+public interface IRecruiterJobs {
+
+    Long getTotalCandidates();
+    int getJob_post_id();
+
+    String getJob_title();
+
+    int getLocationId();
+
+    String getCity();
+    String getState();
+    String getCountry();
+    int getCompanyId();
+    String getName();
+}
--- src/main/java/com/luv2code/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/luv2code/jobportal/services/JobPostActivityService.java
@@ -1,9 +1,16 @@
 package com.luv2code.jobportal.services;
 
+import com.luv2code.jobportal.enityt.IRecruiterJobs;
+import com.luv2code.jobportal.entity.JobCompany;
+import com.luv2code.jobportal.entity.JobLocation;
 import com.luv2code.jobportal.entity.JobPostActivity;
+import com.luv2code.jobportal.entity.RecruiterJobsDto;
 import com.luv2code.jobportal.repository.JobPostActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class JobPostActivityService {
@@ -17,5 +24,17 @@ public class JobPostActivityService {
 
     public JobPostActivity addNew(JobPostActivity jobPostActivity){
         return jobPostActivityRepository.save(jobPostActivity);
+    }
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter){
+        List<IRecruiterJobs> recruiterJobsDtos = jobPostActivityRepository.getRecruiterJobs(recruiter);
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+
+        for(IRecruiterJobs rec : recruiterJobsDtos){
+            //construct DTO based on info from database
+            JobLocation loc = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
+            JobCompany comp = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(), rec.getJob_title(), loc, comp));
+        }
+        return recruiterJobsDtoList;
     }
 }
